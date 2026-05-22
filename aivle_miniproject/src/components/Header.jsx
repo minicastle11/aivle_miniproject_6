b95d00d02c0e2f89fe93bd2de9f25f65789403e7
hanseok <lovegkstjr01@naver.com>
2026-05-22 14:00:40 +0900

    헤더추가

---
+++ aivle_miniproject/src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header className="header">
+      <p className="logo">
+        도서관리
+      </p>
+
+      <p className="nav">
+        <p>홈</p>
+        <p>도서 목록</p>
+        <p>
+          새 도서 등록
+        </p>
+      </p>
+    </header>
+  );
+}
+
+export default Header;
