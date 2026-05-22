2f525a05786420ef5aa0e9917df0ffaaf23ffc80
MUZI <mzui2033@gmail.com>
2026-05-22 16:25:31 +0900

    modified:   src/pages/HomePage.jsx 	modified:   src/pages/ListPage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import db from '../../db.json';
+import { Link } from 'react-router-dom';
 
 function App() {
 
---
+++ src/pages/ListPage.jsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+
+function ListPage({ books }) {
+
+  return (
+    <div>
+
+      <h1>📚 전체 도서 목록</h1>
+
+      <div className="book-list">
+
+        {books.map((book) => (
+
+          <div className="book-card" key={book.id}>
+
+            <img
+              src={book.coverImageUrl}
+              alt={book.title}
+            />
+
+            <h3>{book.title}</h3>
+
+            <p>{book.author}</p>
+
+            <div>
+              ❤️ {book.likes}
+            </div>
+
+            <Link to={`/detail/${book.id}`}>
+
+              <button>
+                자세히 보기
+              </button>
+
+            </Link>
+
+          </div>
+
+        ))}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default ListPage;
